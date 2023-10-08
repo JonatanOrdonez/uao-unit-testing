ad9fb47fc2babc42cb38dd6fa7a0fbfff1c9d138
ImJhojan <jespinosam116@gmail.com>
2023-10-07 21:05:56 -0500

    Testing code moduleTwo by Jhojan

--- src/__tests__/moduleTwo.spec.mjs
+++ src/__tests__/moduleTwo.spec.mjs
@@ -2,10 +2,9 @@ import {
   describe, it, expect, jest,
 } from '@jest/globals';
 
-import { generateFibonacciSequence } from '../moduleTwo.mjs';
+import axios from 'axios';
 
-// Mockear Axios
-jest.mock('axios');
+import { generateFibonacciSequence, getUsers } from '../moduleTwo.mjs';
 
 describe('generateFibonacciSequence', () => {
   describe('generateFibonacciSequence', () => {
@@ -20,5 +19,50 @@ describe('generateFibonacciSequence', () => {
       expect(() => generateFibonacciSequence(0)).toThrow('Limit must be greater than 0');
       expect(() => generateFibonacciSequence(-5)).toThrow('Limit must be greater than 0');
     });
+  });
+});
+
+describe('getUsers', () => {
+  it('usuario ', async () => {
+    const mock = [{ name: 'jhojan' }];
+
+    jest.spyOn(axios, 'get').mockResolvedValueOnce({ status: 200, data: mock });
+    const result = await getUsers();
+    expect(result).toEqual(mock);
+  });
+
+  it('Clementine no aceptado', async () => {
+    const mockData = [{ name: 'Clementine' }];
+
+    jest.spyOn(axios, 'get').mockResolvedValueOnce({ status: 200, data: mockData });
+
+    try {
+      await getUsers();
+    } catch (error) {
+      expect(error.message).toBe('Clementine is not allowed');
+    }
+  });
+
+  it('Clementine no aceptado', async () => {
+    const mockData = [{ name: 'Clementine' }];
+
+    jest.spyOn(axios, 'get').mockResolvedValueOnce({ status: 200, data: mockData });
+
+    try {
+      await getUsers();
+    } catch (error) {
+      expect(error.message).toBe('Clementine is not allowed');
+    }
+  });
+
+  it('No encontrados', async () => {
+    // Configurar el mock de Axios para que responda con una respuesta sin usuarios
+    jest.spyOn(axios, 'get').mockResolvedValueOnce({ status: 200, data: [] });
+
+    try {
+      await getUsers();
+    } catch (error) {
+      expect(error.message).toBe('No users found');
+    }
   });
 });
