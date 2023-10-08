949629db8125182e753f164ba2ad23fdc48bde9c
ImJhojan <jespinosam116@gmail.com>
2023-10-07 19:15:06 -0500

    Testing code moduleTwo by Jhojan

---
+++ src/__tests__/moduleTwo.spec.mjs
@@ -0,0 +1,24 @@
+import {
+  describe, it, test, expect, jest,
+} from '@jest/globals';
+
+import { generateFibonacciSequence } from '../moduleTwo.mjs';
+
+// Mockear Axios
+jest.mock('axios');
+
+describe('generateFibonacciSequence', () => {
+  describe('generateFibonacciSequence', () => {
+    it('debería generar una secuencia de Fibonacci hasta el límite especificado', () => {
+      const limit = 2;
+      const expectedSequence = [0, 1, 1, 2];
+      const result = generateFibonacciSequence(limit);
+      expect(result).toEqual(expectedSequence);
+    });
+
+    it('debería arrojar un error si el límite es menor o igual a 0', () => {
+      expect(() => generateFibonacciSequence(0)).toThrow('Limit must be greater than 0');
+      expect(() => generateFibonacciSequence(-5)).toThrow('Limit must be greater than 0');
+    });
+  });
+});
