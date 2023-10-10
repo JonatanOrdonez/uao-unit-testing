7878532a6d73c54d290676819ab434e9a7258540
Jesus-0sorio <jaoz10.16@gmail.com>
2023-10-10 14:25:05 -0500

    100% code coverage achieved.

--- src/__tests__/ProcessService.spec.mjs
+++ src/__tests__/ProcessService.spec.mjs
@@ -5,12 +5,12 @@ import {
   jest,
 } from '@jest/globals';
 import axios from 'axios';
-import { generateFibonacciSequence, getUsers } from '../../moduleTwo.mjs';
+import { generateFibonacciSequence, getUsers } from '../moduleTwo.mjs';
 
 describe('generateFibonacciSequence', () => {
   test('Positivo: generar secuencia de fibonacci', () => {
-    expect(generateFibonacciSequence(5)).toEqual([0, 1, 1, 2, 3]);
-    expect(generateFibonacciSequence(10)).toEqual([0, 1, 1, 2, 3, 5, 8]);
+    expect(generateFibonacciSequence(5)).toEqual([0, 1, 1, 2, 3, 5]);
+    expect(generateFibonacciSequence(10)).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
   });
 
   test('debería arrojar un error si el límite es menor o igual a 0', () => {
@@ -21,6 +21,10 @@ describe('generateFibonacciSequence', () => {
 
 describe('getUsers', () => {
   test('debería obtener usuarios correctamente', async () => {
+    // Mockear la función axios.get para simular una respuesta con usuarios
+    axios.get = jest.fn().mockResolvedValue({
+      data: [{ name: 'John' }, { name: 'Jane' }],
+    });
     const users = await getUsers();
     expect(users.length).toBeGreaterThan(0);
   });
